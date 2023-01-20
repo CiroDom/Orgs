b3c88cd5f2e941d3efc7a8cb799495538433822e
Ciro de Oliveira <c.dom.oliveira@gmail.com>
2023-01-20 14:56:46 -0300

    correção no layout

--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -58,8 +58,8 @@ class ListaProdutosAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
-        val binding = ProdutoItemBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding)
+        val view = ProdutoItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
